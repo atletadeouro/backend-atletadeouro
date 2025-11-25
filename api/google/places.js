f6db21bfff0f79ca90ed9c9083b74014709e8725
Alexandre Uglar <acuglar@gmail.com>
2025-11-24 23:24:50 -0300

    Add getGooglePlaceReviews

---
+++ api/google/places.js
@@ -0,0 +1,25 @@
+export default async function getGooglePlaceReviews(req, res) {
+  const { placeId } = req.query;
+
+  if (!placeId) {
+    return res.status(400).json({ error: "Missing placeId" });
+  }
+
+  const url =
+    `https://places.googleapis.com/v1/places/${placeId}`;
+
+  const googleRes = await fetch(url,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': process.env.GOOGLE_PLACES_KEY,
+        'X-Goog-FieldMask': 'id,displayName,rating,userRatingCount,reviews'
+      }
+    }
+  );
+
+  const data = await googleRes.json();
+
+  return res.status(200).json(data);
+}
+
